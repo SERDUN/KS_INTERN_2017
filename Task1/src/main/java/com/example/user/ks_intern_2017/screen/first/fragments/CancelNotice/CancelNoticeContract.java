3db2837d542d84bb1f04bbace990832ad9e9fca3
User <dmitriiserdun@gmail.com>
2017-07-26 11:54:36 +0300

    finished home work

--- Task1/src/main/java/com/example/user/ks_intern_2017/screen/first/fragments/CancelNotice/CancelNoticeContract.java
+++
@@ -1,8 +0,0 @@
-package com.example.user.ks_intern_2017.screen.first.fragments.CancelNotice;
-
-/**
- * Created by User on 19.07.2017.
- */
-
-public class CancelNoticeContract {
-}
--- Task1/src/main/java/com/example/user/ks_intern_2017/screen/first/fragments/OkNotice/DataEntryPresenter.java
+++
@@ -1,8 +0,0 @@
-package com.example.user.ks_intern_2017.screen.first.fragments.OkNotice;
-
-/**
- * Created by User on 19.07.2017.
- */
-
-public class DataEntryPresenter {
-}
